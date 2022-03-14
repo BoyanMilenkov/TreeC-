a5fb7cd4761b781da86e9b26dcb666a72f40e96b
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-03-14 18:38:58 +0200

    Add files via upload

---
+++ BFStraversefolders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BFStraversefolders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TraverseDirBFS(@"C:\Windows\assembly");
+        }
+        static void TraverseDirBFS(string directoryPath)
+        {
+            var visitedDirsQueue = new Queue<DirectoryInfo>();
+            visitedDirsQueue.Enqueue(new DirectoryInfo(directoryPath));
+            while (visitedDirsQueue.Count > 0)
+            {
+                DirectoryInfo currentDir = visitedDirsQueue.Dequeue();
+                Console.WriteLine(currentDir.FullName);
+                DirectoryInfo[] children = currentDir.GetDirectories();
+                foreach (DirectoryInfo child in children)
+                {
+                    visitedDirsQueue.Enqueue(child);
+                }
+                   
+
+            }
+        }
+       
+    }
+}
